cb7b6f1b27aa17f0ed82f461a7e0fa69c2ffd9e4
Sean Brown <sbbrown35@gmail.com>
2018-06-19 19:32:55 -0500

    rev 2
    added some thingz

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -5,21 +5,25 @@ module.exports = function(app) {
 
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../../public/index.html"))
    
   });
 
   app.get("/about", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../public/aboutme.html"));
+    res.sendFile(path.join(__dirname, "../../public/aboutme.html"))
   });
 
   app.get("/portfolio", function(req, res){
-    res.sendFile(path.join(__dirname, "../../public/portfolio.html" )); 
+    res.sendFile(path.join(__dirname, "../../public/portfolio.html" )) 
   });
 
-  app.get("/contact", function(req, res, restart){
-    res.sendFile(path.join(__dirname, "../../public/contact.html"));
+  app.get("/contact", function(req, res){
+    res.sendFile(path.join(__dirname, "../../public/contact.html"))
 
+  });
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/home.html"));
   });
 
   app.get("*", function(req, res) {
